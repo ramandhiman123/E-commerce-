363520b932269946853b8e1efb6af6a552cbc3f7
Raman <ramandhimanwins@gmail.com>
2024-03-12 16:33:48 +0530

    Intial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     $this->call(RoleSeeder::class);
     $this->call(AdminSeeder::class);
     $this->call(CategorySeeder::class);
-    $this->call(imageSeeder::class);
+    $this->call(ImageSeeder::class);
 
   }
 }
